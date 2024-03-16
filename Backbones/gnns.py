2f01caf59efce0f9c3c4ddb1418f9d0e94679fc9
AUnal7 <55763145+AUnal7@users.noreply.github.com>
2024-03-17 00:31:37 +0300

    Update gnns.py

--- Backbones/gnns.py
+++ Backbones/gnns.py
@@ -19,6 +19,7 @@ class GIN(nn.Module):
         e_list = []
         h, e = self.gat_layers[0](g, features)
         x = F.relu(h)
+        #e_list = e_list + e
         logits, e = self.gat_layers[1](g, x)
         self.second_last_h = logits if len(self.gat_layers) == 1 else h
         e_list = e_list + e
@@ -28,6 +29,7 @@ class GIN(nn.Module):
         e_list = []
         h, e = self.gat_layers[0].forward_batch(blocks[0], features)
         x = F.relu(h)
+        #e_list = e_list + e
         logits, e = self.gat_layers[1].forward_batch(blocks[1], x)
         self.second_last_h = logits if len(self.gat_layers) == 1 else h
         e_list = e_list + e
@@ -46,6 +48,15 @@ class GIN_original(nn.Module):
         for l in range(len(dims) - 1):
             lin = torch.nn.Linear(dims[l], dims[l + 1])
             self.gat_layers.append(GINConv(lin, 'sum'))
+        '''
+        lin1 = torch.nn.Linear(args.d_data, args.GIN_args['num_hidden'])
+        lin2 = torch.nn.Linear(args.GIN_args['num_hidden'], args.n_cls, bias=False)
+        self.layer1 = GINConv(lin1, 'sum')
+        self.layer2 = GINConv(lin2, 'sum')
+        self.gat_layers = nn.ModuleList()
+        self.gat_layers.append(self.layer1)
+        self.gat_layers.append(self.layer2)
+        '''
 
     def forward(self, g, features):
         e_list = []
@@ -61,6 +72,7 @@ class GIN_original(nn.Module):
         e_list = []
         h, e = self.gat_layers[0].forward_batch(blocks[0], features)
         x = F.relu(h)
+        # e_list = e_list + e
         logits, e = self.gat_layers[1].forward_batch(blocks[1], x)
         self.second_last_h = logits if len(self.gat_layers) == 1 else h
         e_list = e_list + e
@@ -106,6 +118,18 @@ class GCN(nn.Module):
         e_list = e_list + e
         return logits, e_list
 
+    def maml(self, g, features, params):
+        e_list = []
+        h = features
+        for layer, fast in zip(self.gat_layers[:-1], params[:-1]):
+            h, e = layer.maml(g, h, fast)
+            h = F.relu(h)
+            e_list = e_list + e
+            h = F.dropout(h, p=self.dropout, training=self.training)
+        logits, e = self.gat_layers[-1].maml(g, h, params[-1])
+        self.second_last_h = logits if len(self.gat_layers) == 1 else h
+        e_list = e_list + e
+        return logits, e_list
 
     def reset_params(self):
         for layer in self.gat_layers:
@@ -124,7 +148,7 @@ class GAT(nn.Module):
         self.activation = activation
         # input projection (no residual)
         self.gat_layers.append(GATConv(
-            args.d_data, args.GAT_args['num_hidden'], heads[0],
+            args.d_data if args.dataset != "Reddit-CL" else (args.d_data, args.d_data), args.GAT_args['num_hidden'], heads[0],
             args.GAT_args['feat_drop'], args.GAT_args['attn_drop'], args.GAT_args['negative_slope'], False, None))
         # self.norm_layers.append(nn.BatchNorm1d(num_hidden*heads[0]))
         self.norm_layers.append(PairNorm())
@@ -140,29 +164,71 @@ class GAT(nn.Module):
         # output projection
 
         self.gat_layers.append(GATConv(
-            args.GAT_args['num_hidden'] * heads[-2], args.n_cls, heads[-1],
+            args.GAT_args['num_hidden'] * heads[-2] if args.dataset != "Reddit-CL" else (args.GAT_args['num_hidden'] * heads[-2], args.GAT_args['num_hidden'] * heads[-2]), args.n_cls, heads[-1],
             args.GAT_args['feat_drop'], args.GAT_args['attn_drop'], args.GAT_args['negative_slope'], args.GAT_args['residual'], None))
 
     def forward(self, g, inputs, save_logit_name = None):
         h = inputs
         e_list = []
+        
         for l in range(self.num_layers):
             h, e = self.gat_layers[l](g, h)
             h = h.flatten(1)
             h = self.activation(h)
             e_list = e_list + e
+
         # store for ergnn
-        self.second_last_h = h
+        # self.second_last_h = h
+            
         # output projection
         logits, e = self.gat_layers[-1](g, h)
-        #self.second_last_h = logits if len(self.gat_layers) == 1 else h
+
+        self.second_last_h = logits if len(self.gat_layers) == 1 else h
+
         logits = logits.mean(1)
         e_list = e_list + e
+        
         return logits, e_list
+    
+    def forward_batch(self, blocks, inputs):
+        # h = inputs # torch.Size([106373, 602])
+        '''
+        for l in range(self.num_layers):
+            h = self.gat_layers[l](g, h).flatten(1)
+            h = self.activation(h)
+        logits = self.gat_layers[-1](g, h).mean(1)
+        
+        '''    
+        """elist = []
+        prev_h = None
+        for l, (layer, block) in enumerate(zip(self.gat_layers, blocks)):
 
-    def forward_batch(self, blocks, features):
+            # We need to first copy the representation of nodes on the RHS from the
+            # appropriate nodes on the LHS.
+            # Note that the shape of h is (num_nodes_LHS, D) and the shape of h_dst
+            # would be (num_nodes_RHS, D)
+            
+            h_dst = h[:block.number_of_dst_nodes()]  #torch.Size([9687, 602])
+            # Then we compute the updated representation on the RHS.
+            # The shape of h now becomes (num_nodes_RHS, D)
+            h, e = layer.forward_batch(block, (h,h_dst))
+            
+            elist.append(e)
+            
+            if l != len(self.gat_layers) - 1:
+                self.second_last_h = h if len(self.gat_layers) == 1 else prev_h
+                h = h.flatten(1)
+                h = self.activation(h)
+               # h = self.dropout(h)
+            else:
+                h = h.mean(1)
+            
+            prev_h = h
+        return h, elist"""
+
+
         e_list = []
-        h = features
+        h = inputs
         for i,layer in enumerate(self.gat_layers[:-1]):
             h, e = layer.forward_batch(blocks[i], h)
             h = h.flatten(1)
@@ -173,7 +239,6 @@ class GAT(nn.Module):
         logits = logits.mean(1)
         e_list = e_list + e
         return logits, e_list
-
 
     def reset_params(self):
         for layer in self.gat_layers:
